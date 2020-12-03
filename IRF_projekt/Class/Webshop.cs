f44604fe9894c5763ac8f2981de7435b5ae24d61
Botond Hegedüs <hbotond1999@gmail.com>
2020-12-03 12:10:26 +0100

    adatok beolvasása kész

---
+++ IRF_projekt/Class/Webshop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_projekt
+{
+    class Webshop
+    {
+        public int Rendeles_Id { get; set; }
+        public string Login { get; set; }
+        public string Nev { get; set; }
+        public string email { get; set; }
+        public DateTime Szul_Datum { get; set; }
+        public string Telefon { get; set; }
+        public string Cim { get; set; }
+        public DateTime Rend_Ideje { get; set; }
+        public string Rendelt_Termek { get; set; }
+        public int Mennyiseg { get; set; }
+    }
+}
--- IRF_projekt/UserControl1.cs
+++ IRF_projekt/UserControl1.cs
@@ -1,20 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace IRF_projekt
 {
     public partial class UserControl1 : UserControl
     {
+        private List<Webshop> webshops = new List<Webshop>();
         public UserControl1()
         {
             InitializeComponent();
+            Beolvasás();
+
+        }
+
+        private void Beolvasás()
+        {
+
+            using (StreamReader sr = new StreamReader("WebshopVégleges.csv", Encoding.UTF8))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] sor = sr.ReadLine().Split(';');
+
+                    Webshop w = new Webshop();
+                    try
+                    {
+                        w.Rendeles_Id = Convert.ToInt32(sor[0]);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Rendeles_Id beolvasása nem sikerült");
+                        return;
+                    }
+
+                    w.Login = sor[1];
+                    w.Nev = sor[2];
+                    w.email = sor[3];
+                    try
+                    {
+                        w.Szul_Datum = Convert.ToDateTime(sor[4]);
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Szul_Datum beolvasása nem sikerült");
+                        return;
+                    }
+                    w.Telefon = sor[5];
+                    w.Cim = sor[6];
+                    try
+                    {
+                        w.Rend_Ideje = Convert.ToDateTime(sor[7]);
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Rend_Ideje beolvasása nem sikerült");
+                        return;
+                    }
+                    w.Rendelt_Termek = sor[8];
+                    try
+                    {
+                        w.Mennyiseg = Convert.ToInt32(sor[9]);
+                    }
+                    catch (Exception)
+                    {
+
+                        MessageBox.Show("Mennyiseg beolvasása nem sikerült");
+                        return;
+                    }
+
+                    webshops.Add(w);
+
+
+                }
+            }
+
+            dataGridView1.DataSource = webshops.ToList();
+
         }
     }
 }
