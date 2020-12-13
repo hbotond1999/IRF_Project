717b967a63d689bb5f155fdf25313efdb093c5a7
Botond Hegedüs <hbotond1999@gmail.com>
2020-12-13 16:13:40 +0100

    regex

--- IRF_projekt/UserControl2.Designer.cs
+++ IRF_projekt/UserControl2.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Name = "UserControl2";
-            this.Size = new System.Drawing.Size(461, 335);
+            this.Size = new System.Drawing.Size(490, 339);
             this.ResumeLayout(false);
 
         }
